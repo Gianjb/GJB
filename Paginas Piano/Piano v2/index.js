8b978a82331466b17f96a9ead2ac6ad9862ec451
Gianluca Joaquin Baiza <gianlucabaiza@gmail.com>
2023-02-09 21:20:24 -0300

    Primer repositorio

---
+++ Paginas Piano/Piano v2/index.js
@@ -0,0 +1,16 @@
+/*
+let nombre = prompt("¿Como te llamas?");
+
+let saludo = `Bienvenido ${nombre} ya podes ingresar.`;
+
+alert(saludo);
+
+*/
+
+let uno = 23;
+let dos = 25;
+
+let final = uno < dos;
+let final2 = dos > uno;
+
+document.write(final && final2);
